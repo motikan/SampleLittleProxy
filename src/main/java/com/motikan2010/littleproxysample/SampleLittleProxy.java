314e07eb1071ebd1c0e6160dc094705b4c91efb4
KouheiAgena <motikan2010@yahoo.co.jp>
2017-07-25 20:39:19 +0900

    Clean SampleLittleProxy class.

--- src/main/java/com/motikan2010/littleproxysample/SampleLittleProxy.java
+++ src/main/java/com/motikan2010/littleproxysample/SampleLittleProxy.java
@@ -2,6 +2,8 @@ package com.motikan2010.littleproxysample;
 
 import java.net.InetSocketAddress;
 import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
 
 import io.netty.handler.codec.http.*;
 import org.littleshoot.proxy.HttpFilters;
@@ -9,7 +11,6 @@ import org.littleshoot.proxy.HttpFiltersSource;
 import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
 
 import io.netty.channel.ChannelHandlerContext;
-
 
 public class SampleLittleProxy {
 
@@ -30,6 +31,11 @@ public class SampleLittleProxy {
 
     static class SampleHttpFiltersSource implements HttpFiltersSource{
 
+        private final boolean CLIENT_TO_PROXY_REQUEST = true;
+        private final boolean PROXY_TO_SERVER_REQUEST_ENABLE = true;
+        private final boolean SERVER_TO_PROXY_RESPONSE_ENABLE = true;
+        private final boolean PROXY_TO_CLIENT_RESPONSE_ENABLE = true;
+
         @Override
         public int getMaximumResponseBufferSizeInBytes() {
             return 10240 * 10240;
@@ -45,6 +51,98 @@ public class SampleLittleProxy {
             return new HttpFilters() {
 
                 @Override
+                public HttpResponse clientToProxyRequest(HttpObject httpObject) {
+                    if(CLIENT_TO_PROXY_REQUEST == true){
+                        showRequest(httpObject);
+                    }
+
+                    return null;
+                }
+
+                @Override
+                public HttpResponse proxyToServerRequest(HttpObject httpObject) {
+                    if(PROXY_TO_SERVER_REQUEST_ENABLE == true){
+                        showRequest(httpObject);
+                    }
+                    return null;
+                }
+
+                @Override
+                public HttpObject serverToProxyResponse(HttpObject httpObject) {
+                    if(SERVER_TO_PROXY_RESPONSE_ENABLE == true){
+                        showResponse(httpObject);
+                    }
+
+                    return httpObject;
+                }
+
+                @Override
+                public HttpObject proxyToClientResponse(HttpObject httpObject) {
+                    if(PROXY_TO_CLIENT_RESPONSE_ENABLE == true){
+                        showResponse(httpObject);
+                    }
+
+                    return httpObject;
+                }
+
+                private void showRequest(HttpObject httpObject){
+                    if (httpObject instanceof HttpRequest) {
+                        HttpRequest httpRequest = (HttpRequest) httpObject;
+
+                        // メソッド
+                        System.out.print(httpRequest.getMethod() + " ");
+
+                        // URI
+                        System.out.print(httpRequest.getUri() + " ");
+
+                        // HTTPバージョン
+                        System.out.println(httpRequest.getProtocolVersion());
+
+                        // ヘッダー
+                        HttpHeaders httpHeaders = httpRequest.headers();
+                        List<Map.Entry<String,String>> headerList = httpHeaders.entries();
+                        for (Map.Entry<String, String> header : headerList){
+                            System.out.println(header.getKey() + ": " + header.getValue());
+                        }
+                    }
+
+                    if (httpObject instanceof HttpContent) {
+                        HttpContent httpContent = (HttpContent) httpObject;
+                        showContent(httpContent);
+                    }
+                    System.out.println();
+                }
+
+                private void showResponse(HttpObject httpObject){
+                    // レスポンスヘッダ
+                    if (httpObject instanceof HttpResponse) {
+                        System.out.print(((HttpResponse) httpObject).getProtocolVersion() + " ");
+
+                        System.out.println(((HttpResponse) httpObject).getStatus());
+
+                        // ヘッダー
+                        HttpHeaders httpHeaders = ((HttpResponse) httpObject).headers();
+                        List<Map.Entry<String,String>> headerList = httpHeaders.entries();
+                        for (Map.Entry<String, String> header : headerList){
+                            System.out.println(header.getKey() + ": " + header.getValue());
+                        }
+                    }
+
+                    // レスポンスボディ
+                    if (httpObject instanceof HttpContent) {
+                        HttpContent httpContent = (HttpContent) httpObject;
+                        showContent(httpContent);
+                    }
+                    System.out.println();
+                }
+
+                private void showContent(HttpContent httpContent){
+                    String resposeBody = httpContent.content().toString(Charset.defaultCharset());
+                    System.out.println();
+                    System.out.println(resposeBody);
+                }
+
+                @Override
                 public void serverToProxyResponseTimedOut() {}
 
                 @Override
@@ -52,28 +150,6 @@ public class SampleLittleProxy {
 
                 @Override
                 public void serverToProxyResponseReceived() {}
-
-                @Override
-                public HttpObject serverToProxyResponse(HttpObject httpObject) {
-                    System.out.println("=== serverToProxyResponse ===");
-
-                    // レスポンスヘッダ
-                    if (httpObject instanceof HttpResponse) {
-                        HttpResponse httpResponse = (HttpResponse) httpObject;
-                        System.out.println(httpResponse.toString());
-                        System.out.println();
-                    }
-
-                    // レスポンスボディ
-                    if (httpObject instanceof HttpContent) {
-                        HttpContent httpContent = (HttpContent) httpObject;
-                        String resposeBody = httpContent.content().toString(Charset.defaultCharset());
-                        System.out.println(resposeBody);
-                    }
-
-                    System.out.println("");
-                    return httpObject;
-                }
 
                 @Override
                 public void proxyToServerResolutionSucceeded(String serverHostAndPort, InetSocketAddress resolvedRemoteAddress) {}
@@ -93,16 +169,6 @@ public class SampleLittleProxy {
                 public void proxyToServerRequestSending() {}
 
                 @Override
-                public HttpResponse proxyToServerRequest(HttpObject httpObject) {
-                    System.out.println("=== proxyToServerRequest ===");
-                    if (httpObject instanceof HttpRequest) {
-                        System.out.println(httpObject.toString());
-                    }
-                    System.out.println("");
-                    return null;
-                }
-
-                @Override
                 public void proxyToServerConnectionSucceeded(ChannelHandlerContext serverCtx) {}
 
                 @Override
@@ -116,38 +182,6 @@ public class SampleLittleProxy {
 
                 @Override
                 public void proxyToServerConnectionFailed() {}
-
-                @Override
-                public HttpObject proxyToClientResponse(HttpObject httpObject) {
-                    System.out.println("=== proxyToClientResponse ===");
-
-                    // レスポンスヘッダ
-                    if (httpObject instanceof HttpResponse) {
-                        HttpResponse httpResponse = (HttpResponse) httpObject;
-                        System.out.println(httpResponse.toString());
-                        System.out.println();
-                    }
-
-                    // レスポンスボディ
-                    if (httpObject instanceof HttpContent) {
-                        HttpContent httpContent = (HttpContent) httpObject;
-                        String resposeBody = httpContent.content().toString(Charset.defaultCharset());
-                        System.out.println(resposeBody);
-                    }
-
-                    System.out.println("");
-                    return httpObject;
-                }
-
-                @Override
-                public HttpResponse clientToProxyRequest(HttpObject httpObject) {
-                    System.out.println("=== clientToProxyRequest ===");
-                    if (httpObject instanceof HttpRequest) {
-                        System.out.println(httpObject.toString());
-                    }
-                    System.out.println("");
-                    return null;
-                }
 
             };
         }
